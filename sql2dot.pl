e50d1e3b4aa4396a3576354693a3fd7a7cf8cd50
Daniel Gray <the.daniel.gray@gmail.com>
2020-03-07 13:55:15 -0400

    Fix underscore fields not being correctly considered

--- sql2dot.pl
+++ sql2dot.pl
@@ -1,20 +1,21 @@
 #!/usr/bin/perl
 
 print "digraph ER_diagram {\n";
+print "graph [rankdir=\"BT\"];\n";
 my $current_table;
 while (<STDIN>) {
 	chomp;
-	if (m/CREATE TABLE ([A-Za-z0-9]*) \(/) {
+	if (m/CREATE TABLE ([A-Za-z0-9_]*) \(/) {
 		$current_table = $1;
 		@fields = ();
 		@references = ();
 	}
 	my $current_field_name;
-	if (m/^\s+,?([a-zA-Z0-9]*)\s+[^\s]+/) {
+	if (m/^\s+,?([a-zA-Z0-9_]*)\s+[^\s]+/) {
 		$current_field_name = $1;
 		push @fields,$current_field_name;
 	}
-	if (m/REFERENCES ([A-Za-z0-9]+)\(([a-zA-Z0-9]+)\)/) {
+	if (m/REFERENCES ([A-Za-z0-9_]+)\(([a-zA-Z0-9_]+)\)/i) {
 		push @references, "\t$current_table:$current_field_name -> $1:$2;\n";
 	}
 	if (m/^\);/) {
